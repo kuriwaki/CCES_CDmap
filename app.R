7b71e0388dc765a07a0486b3a58ce2898834a026
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2021-02-19 01:13:04 -0500

    Major overhaul -- include rpv

--- app.R
+++ app.R
@@ -9,7 +9,30 @@ library(tidyverse)
 library(sf)
 library(glue)
 library(scales)
+library(janitor)
 
+# Base data
+cd_race <- cd_shp %>%
+  left_join(race_by_cd, by = "cd") %>%
+  left_join(cd_info, by = "cd") %>%
+  mutate(across(starts_with("pct_"), ~percent(.x, accuracy = 1)))
+st_crs(cd_race) <- 3857
+
+cc_rpv <- read_rds("data/by-cd-race2.rds")
+shp_rpv <- cd_shp %>%
+  left_join(cc_rpv, by = "cd") %>%
+  mutate(across(starts_with("pct_"), ~percent(.x, accuracy = 1)))
+st_crs(shp_rpv) <- 3857
+
+cc_diff <- cc_rpv %>%
+  pivot_wider(id_cols = c(cd, pct_trump, pct_white), names_from = race2, values_from = p_mrp_est) %>%
+  clean_names()
+shp_diff <- cd_shp %>%
+  left_join(cc_diff, by = "cd") %>%
+  mutate(across(starts_with("pct_"), ~percent(.x, accuracy = 1)))
+st_crs(shp_diff) <- 3857
+
+mrp_range <- range(cc_rpv$p_mrp_est)
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
@@ -29,17 +52,29 @@ ui <- shinyUI(fluidPage(
                         ')),
 
   # Application title
-  # titlePanel("Congressional District Map"),
-
-  # Sidebar with input on race to show
-  selectInput("race", "Racial Group",
-              unique(race_by_cd$race)
+  titlePanel("Racial Voting by Congressional District"),
+  em("Shiro Kuriwaki (https://github.com/kuriwaki/CCES_CDmap). Map from Daniel Donner (http://dkel.ec/map). MRP Estimates by Shiro Kuriwaki."),
+  hr(),
+  tabsetPanel(
+    tabPanel("Race and Racially Polarized Voting",
+            column(4,
+                   selectInput("race", "Show Prevalence of", unique(race_by_cd$race)),
+                   plotlyOutput("cdmap")),
+            column(8,
+                   br(),
+                   strong("Pr(Trump | White) - Pr(Trump | Non-White)"),
+                   br(),
+                   plotlyOutput("diff_trump"))
+    ),
+    tabPanel("Component Estimates",
+             column(6,
+                    strong("Trump vote among Whites"),
+                    plotlyOutput("white_trump")),
+             column(6,
+                    strong("Trump vote among Non-Whites"),
+                    plotlyOutput("black_trump"))
+    )
   ),
-
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotlyOutput("cdmap")
-  )
 )
 )
 
@@ -47,47 +82,95 @@ ui <- shinyUI(fluidPage(
 server <- shinyServer(function(input, output) {
 
   output$cdmap <- renderPlotly({
-
-    cd_race <- cd_shp %>%
-      # add race data
-      left_join(race_by_cd, by = "cd") %>%
+    cd_race <- cd_race %>%
       filter(race == input$race) %>%
-      # add CD info
-      left_join(cd_info, by = "cd") %>%
-      mutate(across(starts_with("pct_"), ~percent(.x, accuracy = 1))) %>%
-      # Make label
       mutate(cd_lab = glue("<b>{cd}</b><br>{dailykos_name}<br>",
                            "{ush116_rep} ({ush116_party})<br>",
                            "{input$race}: {percent(frac, accuracy = 1)}<br>",
                            "McCain {pct_mccain}<br>Romney {pct_romney}<br>Trump {pct_trump}"))
 
-    st_crs(cd_race) = 3857
 
     p <- ggplot(cd_race, aes(fill = frac, text = cd_lab)) +
       geom_sf(color = "white", size = 0.1) +
       scale_fill_viridis_c(option = "viridis", limits = c(0, 1), labels = percent_format(accuracy = 1)) +
       theme_map() +
-      labs(fill = input$race)
+      labs(fill = input$race) +
+      guides(fill = FALSE)
 
     ggplotly(p,
              tooltip = "text",
-             colors = "Viridis",
-             width = (0.9*as.numeric(input$dimension[1])),
-             height = (0.9*as.numeric(input$dimension[2]))
+             colors = "Viridis"
+             # width = (0.9*as.numeric(input$dimension[1])),
+             # height = (0.9*as.numeric(input$dimension[2]))
              ) %>%
-      style(hoverlabel = list(bgcolor = "white"), hoveron = "fill") %>%
-      layout(margin = list(b = 20), ##bottom margin in pixels
-             annotations =
-               list(x = 0.1, y = 0.2,
-                    text = "Shiro Kuriwaki (https://github.com/kuriwaki/CCES_CDmap).<br>Map from Daniel Donner (http://dkel.ec/map).<br>Election Data from DailyKos, Race Data from 2018 ACS.",
-                    showarrow = FALSE,
-                    xref = 'paper', yref = 'paper',
-                    align = 'left',
-                    xanchor = 'left', yanchor = 'bottom',
-                    xshift = 0, yshift = 0,
-                    font = list(size = 8))
-      ) %>%
-      plotly_build()
+      style(hoverlabel = list(bgcolor = "white"), hoveron = "fill")
+      # layout(margin = list(b = 20), ##bottom margin in pixels
+      #        annotations =
+      #          list(x = 0.1, y = 0.2,
+      #               text = "",
+      #               showarrow = FALSE,
+      #               xref = 'paper', yref = 'paper',
+      #               align = 'left',
+      #               xanchor = 'left', yanchor = 'bottom',
+      #               xshift = 0, yshift = 0,
+      #               font = list(size = 8))
+      # ) %>%
+      # plotly_build()
+  })
+
+  output$white_trump <- renderPlotly({
+
+    white_rpv <- shp_rpv %>%
+      filter(race2 == "White") %>%
+      mutate(cd_lab = glue("<b>{cd}</b><br>",
+                           "Pr(Trump | White): {percent(p_mrp_est, accuracy = 1)}<br>"))
+
+    p <- ggplot(white_rpv, aes(fill = p_mrp_est, text = cd_lab)) +
+      geom_sf(color = "white", size = 0.1) +
+      scale_fill_fermenter(palette = "Reds", limits = c(0, 0.75), direction = 1) +
+      theme_map() +
+      labs(fill = NULL) +
+      theme(legend.position = "bottom") +
+      guides(fill = FALSE)
+
+    ggplotly(p, tooltip = "text") %>%
+      style(hoverlabel = list(bgcolor = "white"), hoveron = "fill")
+  })
+
+  output$black_trump <- renderPlotly({
+
+    nonwhite_rpv <- shp_rpv %>%
+      filter(race2 == "Non-White") %>%
+      mutate(cd_lab = glue("<b>{cd}</b><br>",
+                           "Pr(Trump | Non-White): {percent(p_mrp_est, accuracy = 1)}<br>"))
+
+    p <- ggplot(nonwhite_rpv, aes(fill = p_mrp_est, text = cd_lab)) +
+      geom_sf(color = "white", size = 0.1) +
+      scale_fill_fermenter(palette = "Reds", limits = c(0, 0.75), direction = 1) +
+      theme_map() +
+      guides(fill = FALSE)
+
+    ggplotly(p, tooltip = "text") %>%
+      style(hoverlabel = list(bgcolor = "white"), hoveron = "fill")
+  })
+
+  output$diff_trump <- renderPlotly({
+
+    diff_rpv <- shp_diff %>%
+      mutate(p_mrp_est = white - non_white) %>%
+      mutate(cd_lab = glue("<b>{cd}</b><br>",
+                           "Diff: {percent(p_mrp_est, accuracy = 1, suffix = 'pp')}<br>",
+                           "White {percent(white, accuracy = 1)} - Non-white {percent(non_white, accuracy = 1)}"))
+
+    p <- ggplot(diff_rpv, aes(fill = p_mrp_est, text = cd_lab)) +
+      geom_sf(color = "white", size = 0.1) +
+      scale_fill_distiller(palette = "YlOrRd", direction = 1) +
+      theme_map() +
+      labs(fill = "Race Gap") +
+      guides(fill = FALSE)
+
+    ggplotly(p, tooltip = "text") %>%
+      style(hoverlabel = list(bgcolor = "white"), hoveron = "fill")
   })
 
 })
